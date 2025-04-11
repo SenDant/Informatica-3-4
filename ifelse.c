bcc1a3309491c729355988a72c9a4f9cac7c9900
SenDant <haizh310@gmail.com>
2025-04-11 14:49:24 +0000

    wrong

--- ifelse.c
+++ ifelse.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-    //are these integers equal?
+    // 1. are these integers equal?
     int num1;
     int num2;
 
@@ -11,7 +11,7 @@ int main() {
         printf("%d and %d are equal.\n", num1, num2);
     }
     else { printf("%d and %d are not equal.\n", num1, num2);}
-    //is this integer an odd or even number?
+    // 2. is this integer an odd or even number?
     int num;
     int two = 2;
     int zero = 0;
@@ -24,12 +24,36 @@ int main() {
     else {
         printf("%d is an odd number.\n", num);
     }
-    //what
+    // 3. ????
 
     //Calculator
     printf("This is a calculator of two integers.\n");
-    int
+    int data1;
+    int data2;
+    char symbol;
+    char sum = "+";
+    char sub = "-";
+    char div = "/";
+    char mul = "x";
+    int add = (data1 + data2);
+    int subs = (data1 - data2);
+    int divi = (data1/data2);
+    int multi = (data1*data2);
+    printf("Please enter your operation: \n");
+    scanf("%d %c %d", data1, symbol, data2);
 
+        if(symbol == sum); {
+        printf("%d + %d is %d.", data1, data2, add);
+        }
+        else if(symbol == sub) {
+            printf("%d - %d is %d.", data1, data2, subs);
+        }
+        else if (symbol == div) {
+            printf("%d/%d is %d.", data1, data2, divi);
+        }
+        else if ();
+
+    
 
     return 0;
 }
