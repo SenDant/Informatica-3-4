8c92a42eaf0af5d9b10ca845622a6701e7daf264
SenDant <haizh310@gmail.com>
2025-05-02 19:26:49 +0000

    bl

---
+++ for-loops.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+int n;
+int i;
+int sum;
+printf("Please enter a positive number: ");
+scanf("%d", &n);
+for(i = 1; i <= n; ++i) {
+    sum = (i); //needs fixing.
+    }
+printf("%d", sum);
+    return 0;
+}
