54276df04ef7f6e41ece15b68a8bf63178d940a0
SenDant <haizh310@gmail.com>
2025-05-02 19:14:06 +0000

    challif

---
+++ if-challenge.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int year;
+    printf("Please enter a year: ");
+    scanf(" %d", &year);
+    if(year % 4 == 0, year % 100 != 0 , year % 400 == 0) {
+printf("%d is a leap year.\n", year);
+    } else {
+        printf("%d is NOT a leap year.\n", year);
+    }
+
+}
